11277d6654b999aec4c22b38696bf9a6b4a8ba3f
Marcelo Augusto <maaprofit@gmail.com>
2022-03-02 11:20:46 -0300

    feat: accept database env variavble from heroku

--- app/database.js
+++ app/database.js
@@ -1,9 +1,4 @@
-module.exports = {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+const credentials = {
     dialect: 'postgres',
     dialectOptions: {
         useUTC: true,
@@ -16,4 +11,16 @@ module.exports = {
         idle: 10000,
         acquire: 60 * 60 * 1000
     }
-};
+}
+
+if (process.env.NODE_ENV == 'production') {
+    credentials.url = process.env.DATABASE
+} else {
+    credentials.username = process.env.DB_USER
+    credentials.password = process.env.DB_PASS
+    credentials.database = process.env.DB_NAME
+    credentials.host = process.env.DB_HOST
+    credentials.port = process.env.DB_PORT
+}
+
+module.exports = credentials
